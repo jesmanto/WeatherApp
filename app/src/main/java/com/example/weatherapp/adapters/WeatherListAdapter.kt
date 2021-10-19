e395bc06db4f42e4277ce693cdb10ec6f1dabc91
jesmanto <okorojesse@gmail.com>
2021-10-19 20:22:58 +0100

    Code updates

--- app/src/main/java/com/example/weatherapp/adapters/WeatherListAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapters/WeatherListAdapter.kt
@@ -11,11 +11,13 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WeatherListAdapter @Inject constructor() : RecyclerView.Adapter<WeatherListAdapter.WeatherViewHolder>(){
+class WeatherListAdapter @Inject constructor() :
+    RecyclerView.Adapter<WeatherListAdapter.WeatherViewHolder>() {
     private var cities = emptyList<CityWeatherReport>()
     private var weatherClickListener: Callback<Int>? = null
 
-    inner class WeatherViewHolder(private val binding: CityItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class WeatherViewHolder(private val binding: CityItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(city: CityWeatherReport, listener: View.OnClickListener) {
             binding.viewClickListener = listener
             binding.city = city
@@ -23,29 +25,52 @@ class WeatherListAdapter @Inject constructor() : RecyclerView.Adapter<WeatherLis
         }
     }
 
+    /**
+     * Called when RecyclerView needs a new RecyclerView.ViewHolder
+     * of the given type to represent an item.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        return WeatherViewHolder(CityItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return WeatherViewHolder(
+            CityItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
         val city = cities[position]
-        holder.bind(city,createWeatherClickListener(position))
+        holder.bind(city, createWeatherClickListener(position))
     }
 
     override fun getItemCount(): Int {
         return cities.size
     }
 
-    fun loadCities(citiesList: List<CityWeatherReport>){
+    /**
+     * Adds new items to the empty list of CityWeatherReport
+     * @param citiesList
+     */
+    fun loadCities(citiesList: List<CityWeatherReport>) {
         cities = citiesList
         notifyDataSetChanged()
     }
-    private fun createWeatherClickListener(position: Int): View.OnClickListener{
+
+    /**
+     * Creates the click listener that handles single item click
+     * @param position
+     */
+    private fun createWeatherClickListener(position: Int): View.OnClickListener {
         return View.OnClickListener {
             weatherClickListener?.invoke(position)
         }
     }
-    fun setWeatherClickListener(clickListener: Callback<Int>){
+
+    /**
+     * Sets the click listener to the variable in the layout xml
+     */
+    fun setWeatherClickListener(clickListener: Callback<Int>) {
         this.weatherClickListener = clickListener
     }
 }
